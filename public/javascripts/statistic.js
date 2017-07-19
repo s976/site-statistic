2f41c9f3caff8512b2b3b38ac462d26c47b90c3c
Shimon <shimon.shv@gmail.com>
2017-07-19 20:49:03 +0300

    первый запуск

--- public/javascripts/statistic.js
+++ public/javascripts/statistic.js
@@ -1,15 +1,14 @@
 jQuery.ajax({
-    url: 'http://localhost:8080/api/record',
+    //url: 'http://localhost:8080/api/record',
+    url: 'http://185.14.186.110:8080/api/record',
     method: 'post',
     crossDomain: true,
-    data : {kuku: 'reku'},
+    //data : {kuku: 'reku'},
     dataType : 'json',
     success : function (data, textStatus, jqXHR) {
-        console.log('ura');
-        console.log(data);
+        //console.log(data);
     },
     error : function (data, textStatus, errorThrown) {
-        console.log('ne rabotaet');
         console.log(errorThrown);
     }
 });
